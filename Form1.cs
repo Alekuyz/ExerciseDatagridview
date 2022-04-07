bc596296488bd1fc433f63768db1c1d0d500a31b
Alekuyz <axelagia@gmail.com>
2022-04-07 15:28:18 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExerciseDatagridview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'prodiTIDataSet1.mahasiswa' table. You can move, or remove it, as needed.
+            this.mahasiswaTableAdapter1.Fill(this.prodiTIDataSet1.mahasiswa);
+            // TODO: This line of code loads data into the 'prodiTIDataSet.Mahasiswa' table. You can move, or remove it, as needed.
+            this.mahasiswaTableAdapter.Fill(this.prodiTIDataSet.Mahasiswa);
+
+            dataGridView1.EnableHeadersVisualStyles = false;
+
+            dataGridView1.Columns[3].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            
+
+           
+            
+        }
+
+        private void dataGridView1_CellMouseMove(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            dataGridView1.Columns[0].DefaultCellStyle.SelectionBackColor = Color.HotPink;
+            dataGridView1.Columns[0].DefaultCellStyle.SelectionForeColor = Color.White;
+
+            dataGridView1.Columns[1].DefaultCellStyle.SelectionBackColor = Color.Orange;
+            dataGridView1.Columns[1].DefaultCellStyle.SelectionForeColor = Color.White;
+
+            dataGridView1.Columns[2].DefaultCellStyle.SelectionBackColor = Color.Green;
+            dataGridView1.Columns[2].DefaultCellStyle.SelectionForeColor = Color.White;
+
+            dataGridView1.Columns[3].DefaultCellStyle.SelectionBackColor = Color.Blue;
+            dataGridView1.Columns[3].DefaultCellStyle.SelectionForeColor = Color.White;
+
+            dataGridView1.Columns[4].DefaultCellStyle.SelectionBackColor = Color.Maroon;
+            dataGridView1.Columns[4].DefaultCellStyle.SelectionForeColor = Color.White;
+        }
+
+        private void dataGridView1_CellMouseLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.Columns[0].DefaultCellStyle.SelectionBackColor = Color.White;
+            dataGridView1.Columns[0].DefaultCellStyle.SelectionForeColor = Color.Black;
+
+            dataGridView1.Columns[1].DefaultCellStyle.SelectionBackColor = Color.White;
+            dataGridView1.Columns[1].DefaultCellStyle.SelectionForeColor = Color.Black;
+
+            dataGridView1.Columns[2].DefaultCellStyle.SelectionBackColor = Color.White;
+            dataGridView1.Columns[2].DefaultCellStyle.SelectionForeColor = Color.Black;
+
+            dataGridView1.Columns[3].DefaultCellStyle.SelectionBackColor = Color.White;
+            dataGridView1.Columns[3].DefaultCellStyle.SelectionForeColor = Color.Black;
+
+            dataGridView1.Columns[4].DefaultCellStyle.SelectionBackColor = Color.White;
+            dataGridView1.Columns[4].DefaultCellStyle.SelectionForeColor = Color.Black;
+        }
+    }
+}
